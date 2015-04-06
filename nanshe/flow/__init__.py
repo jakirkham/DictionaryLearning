8b55fa70cd14bdcfee8d75b29b682accb49da041
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-06 14:09:24 -0400

    nanshe/flow/learner.py: Added a stub for the single block learner. nanshe/flow/__init__.py: Added the module `learner`.

--- nanshe/flow/__init__.py
+++ nanshe/flow/__init__.py
@@ -10,6 +10,7 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 06, 2015 13:56:21 EDT$"
 
 
-__all__ = [""]
+__all__ = ["learner"]
 
 
+import learner
---
+++ nanshe/flow/learner.py
@@ -0,0 +1,4 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Apr 06, 2015 14:08:58 EDT$"
+
+
