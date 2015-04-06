047405d62ea188ad2901f32c12c9d1713274b0d8
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-04-06 15:08:16 -0400

    tests/test_nanshe/test_flow/test_learner.py: Added a stub for testing the `ruffus`-based workflow for `nanshe.learner.generate_neurons`. tests/test_nanshe/test_flow/__init__.py: Added the module `test_learner`.

--- tests/test_nanshe/test_flow/__init__.py
+++ tests/test_nanshe/test_flow/__init__.py
@@ -1,6 +1,7 @@
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Apr 06, 2015 14:01:31 EDT$"
 
-__all__ = [""]
+__all__ = ["test_learner"]
 
 
+import test_learner
---
+++ tests/test_nanshe/test_flow/test_learner.py
@@ -0,0 +1,4 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Apr 06, 2015 15:07:46 EDT$"
+
+
