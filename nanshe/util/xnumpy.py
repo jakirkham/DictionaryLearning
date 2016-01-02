978512dc8457755eb90b3857903cc69f607ca511
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-01-02 16:45:42 -0500

    nanshe/util/xnumpy.py: A slightly faster construction of the full array.

--- nanshe/util/xnumpy.py
+++ nanshe/util/xnumpy.py
@@ -4568,7 +4568,7 @@ def pair_dot_product_partially_normalized(new_vector_set, ord=2):
     # Measure the dot product between any two neurons
     # (i.e. related to the angle of separation)
     vector_pairs_dot_product = scipy.linalg.blas.dsyrk(1, new_vector_set)
-    vector_pairs_dot_product += numpy.triu(vector_pairs_dot_product, 1).T
+    vector_pairs_dot_product += vector_pairs_dot_product.T - numpy.diag(vector_pairs_dot_product.diagonal())
 
     vector_pairs_dot_product_normalized = vector_pairs_dot_product / new_vector_set_norms_expanded
 
@@ -4764,7 +4764,7 @@ def pair_dot_product_normalized(new_vector_set, ord=2):
     # Measure the dot product between any two neurons
     # (i.e. related to the angle of separation)
     vector_pairs_dot_product = scipy.linalg.blas.dsyrk(1, new_vector_set)
-    vector_pairs_dot_product += numpy.triu(vector_pairs_dot_product, 1).T
+    vector_pairs_dot_product += vector_pairs_dot_product.T - numpy.diag(vector_pairs_dot_product.diagonal())
 
     # Measure the dot product between any two neurons
     # (i.e. related to the angle of separation)
